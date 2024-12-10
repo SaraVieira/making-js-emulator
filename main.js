e225ce9900305d1c42865aba2b93944c9923d0cd
SaraVieira <hey@iamsaravieira.com>
2024-12-10 20:00:18 +0000

    add more talk lol

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 import Notes from 'reveal.js/plugin/notes/notes.esm'
+import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm'
 import './styles.css'
 import Reveal from 'reveal.js'
 
 const deck = new Reveal()
 deck.initialize({
-  plugins: [Notes],
+  plugins: [Notes, RevealHighlight],
   hash: true,
   slideNumber: true,
   controls: false,
