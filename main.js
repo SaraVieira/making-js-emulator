a1cc99cd3265d58fa7e7266aad788e3f77085287
SaraVieira <hey@iamsaravieira.com>
2024-12-12 16:06:53 +0100

    morr css fixes

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
 import Notes from 'reveal.js/plugin/notes/notes.esm'
 import RevealHighlight from 'reveal.js/plugin/highlight/highlight.esm'
+import 'reveal.js-appearance/plugin/appearance/appearance.css'
+import Appearance from 'reveal.js-appearance/plugin/appearance/appearance.js'
 import './styles.css'
 import Reveal from 'reveal.js'
+import 'animate.css'
 
 const deck = new Reveal()
 deck.initialize({
-  plugins: [Notes, RevealHighlight],
+  plugins: [Notes, RevealHighlight, Appearance],
   hash: true,
   slideNumber: true,
   controls: false,
