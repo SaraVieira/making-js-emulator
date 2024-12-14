031459cfc53f44b7baf6d144b537acf7eba5eab4
SaraVieira <hey@iamsaravieira.com>
2024-12-14 13:44:04 +0100

    fix bitwise

--- Slides/Bitwise.tsx
+++ Slides/Bitwise.tsx
@@ -8,7 +8,7 @@ export const Bitwise = () => {
           <span className="text-white">Bitwise operators</span>
           <p className="fragment font-mono">& {'->'} 0x12 & 0x10 = 0x10</p>
           <p className="fragment font-mono">| {'->'} 0x12 | 0x10 = 0x12</p>
-          <p className="fragment font-mono">^ {'->'} 0x12 & 0x10 = 0x2</p>
+          <p className="fragment font-mono">^ {'->'} 0x12 ^ 0x10 = 0x2</p>
         </div>
       </section>
     </>
